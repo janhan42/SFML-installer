c6dadc612c84966140e6648f3e86f5a535785f1d
janhan42 <janhan@student.42seoul.kr>
2023-12-19 16:30:42 +0900

    add clangd_gen

---
+++ clangd_gen.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sfml_path="../SFML/include"
+
+if [ ! -d "$sfml_path" ]; then
+	echo "SFML library should exists in $sfml_path"
+	exit 1
+fi
+
+pwd=$(pwd)
+
+echo "CompileFlags:" > .clangd
+echo " Add: [-I$pwd/$sfml_path, -I$pwd/src, -std=c++14]" >> .clangd
