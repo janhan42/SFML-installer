a022aeb022fdeb05d7938ddfe743f850a2fdb87a
jang hun han <84340857+janhan42@users.noreply.github.com>
2023-12-21 01:03:33 +0900

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,6 +6,12 @@ compiler=$(cc --version)
 arch=$(uname -m)
 pwd=$(pwd)
 
+NONE='\033[0m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+GRAY='\033[2;37m'
+CURSIVE='\033[3m'
+
 if [[ $compiler == *"gcc"* ]] && [ "$arch" = "x86_64" ]; then
 	sfml_lib="SFML-2.6.1-linux-gcc-64-bit.tar.gz"
 elif [[ $compiler == *"clang"* ]] && [ "$arch" = "x86_64" ]; then
@@ -17,7 +23,12 @@ fi
 sfml_dir="SFML"
 sfml_out=${sfml_lib%.tar.gz}
 
-tar -xzvf ./SFML-installer/lib/SFML/"$sfml_lib" -C "$pwd"
+echo $CURSIVE$GRAY "     - Making SFML..." $NONE
+tar -xzf ./SFML-installer/lib/SFML/"$sfml_lib" -C "$pwd"
+sleep 0.2
+echo -e "\033[1A\033[K"
+sleep 0.2
+
 mv "$sfml_out" "$sfml_dir"
 mv ./"$sfml_dir"/extlibs/FLAC.framework ./"$sfml_dir"/Frameworks/FLAC.framework
 mv ./"$sfml_dir"/extlibs/freetype.framework ./"$sfml_dir"/Frameworks/freetype.framework
@@ -27,3 +38,5 @@ mv ./"$sfml_dir"/extlibs/vorbisenc.framework ./"$sfml_dir"/Frameworks/vorbisenc.
 mv ./"$sfml_dir"/extlibs/vorbisfile.framework ./"$sfml_dir"/Frameworks/vorbisfile.framework
 bash ./SFML-installer/clangd_gen.sh
 rm -fr "SFML-installer"
+echo -e "\033[1A\033[K"
+echo $CURSIVE$GREEN "     - Complite SFML -" $NONE
